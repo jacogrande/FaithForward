9d784f78f014bdc79c651745ea1f51d3f8f29c45
Phaedrus Raznikov <phaedrus.raznikov@memre.ai>
2023-01-05 08:54:59 -0800

    Add policies

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,7 +1,18 @@
 import React from "react";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Linking,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { auth } from "../../firebase";
 import colors from "../styles/colors";
+
+const PRIVACY_POLICY_URL =
+  "https://www.github.com/jacogrande/FaithForward/blob/master/privacy-policy.md";
+const TERMS_OF_SERVICE_URL =
+  "https://www.github.com/jacogrande/FaithForward/blob/master/terms-of-service.md";
 
 // TODO: Unstub account level
 const ProfileScreen: React.FC = () => {
@@ -9,17 +20,35 @@ const ProfileScreen: React.FC = () => {
     <View style={styles.container}>
       <View style={styles.section}>
         <Text style={styles.header}>Profile</Text>
-        <Text style={styles.heading}>
-          {/* Email: {auth.currentUser?.email} */}
-          Email
-        </Text>
+        <Text style={styles.heading}>Email</Text>
         <Text style={styles.text}>{auth.currentUser?.email}</Text>
         <Text style={styles.heading}>Account Level</Text>
         <Text style={styles.text}>Free</Text>
       </View>
       <TouchableOpacity style={styles.button} onPress={() => auth.signOut()}>
         <Text style={styles.buttonText}>Sign Out</Text>
-        {/* <Button title="Sign Out" onPress={() => auth.signOut()} /> */}
+      </TouchableOpacity>
+      <Policies />
+    </View>
+  );
+};
+
+const Policies = () => {
+  const goToPrivacyPolicy = () => {
+    Linking.openURL(PRIVACY_POLICY_URL);
+  };
+
+  const goToTermsOfService = () => {
+    Linking.openURL(TERMS_OF_SERVICE_URL);
+  };
+
+  return (
+    <View style={styles.policies}>
+      <TouchableOpacity onPress={goToPrivacyPolicy}>
+        <Text style={styles.policyLink}>Privacy Policy</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={goToTermsOfService}>
+        <Text style={styles.policyLink}>Terms of Service</Text>
       </TouchableOpacity>
     </View>
   );
@@ -32,6 +61,15 @@ const styles = StyleSheet.create({
     textAlign: "left",
     backgroundColor: colors.paper,
     paddingTop: 80,
+  },
+  policies: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "flex-end",
+  },
+  policyLink: {
+    fontSize: 12,
   },
   section: {
     marginBottom: 16,
