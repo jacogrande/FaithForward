0fa2bc975565c93ffa35597d8819d9fb6351fa88
Phaedrus Raznikov <phaedrus.raznikov@memre.ai>
2023-02-25 12:35:36 -0600

    Use a bit of Tailwind on AuthScreen

--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -9,7 +9,7 @@ import {
   EmailAuthProvider,
   linkWithCredential,
   sendPasswordResetEmail,
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword,
 } from "firebase/auth";
 import React, { useState } from "react";
 import {
@@ -24,7 +24,7 @@ import {
   TextInput,
   TouchableOpacity,
   TouchableWithoutFeedback,
-  View
+  View,
 } from "react-native";
 import { Snackbar } from "react-native-paper";
 
@@ -106,27 +106,26 @@ export const AuthScreen = () => {
       {/* I could not for the life of me color the entire screen without using this. */}
       <LinearGradient
         colors={[colors.paper, colors.paper]}
-        style={styles.linearGradient}
+        className="flex-1"
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
       >
-        <SafeAreaView style={styles.container}>
-          <View style={styles.brandContainer}>
+        <SafeAreaView className="flex-1 justify-center items-center">
+          <View className="flex-1 items-center">
             <Image
               source={require("@assets/church.png")}
               resizeMode="contain"
               style={styles.logo}
             />
-            <View style={styles.faithForward}>
-              <Text style={styles.faithForwardText}>Faith Forward</Text>
+            <View className="mt-5">
+              <Text className="text-4xl font-semibold">Faith Forward</Text>
             </View>
           </View>
           <KeyboardAvoidingView
-            style={styles.content}
             behavior={Platform.OS === "ios" ? "padding" : "height"}
           >
-            <View style={styles.authCard}>
-              <View style={styles.inputs}>
+            <View className="pt-10 pb-5 bg-ffPaper">
+              <View className="pb-5">
                 <TextInput
                   value={email}
                   onChangeText={setEmail}
@@ -172,7 +171,7 @@ export const AuthScreen = () => {
                   </Text>
                 </TouchableOpacity>
               </View>
-              <View style={styles.forgotPasswordView}>
+              <View>
                 <TouchableOpacity
                   style={[
                     styles.forgotPasswordButton,
@@ -231,37 +230,10 @@ const styles = StyleSheet.create({
     paddingBottom: 12,
     backgroundColor: colors.paper,
   },
-  brandContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 50,
-  },
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 40,
-    backgroundColor: colors.paper,
-  },
-  content: {
-    flex: 1,
-    justifyContent: "center",
-    // minHeight: height,
-  },
   error: {
     color: "red",
     textAlign: "center",
   },
-  faithForward: {
-    marginBottom: 40,
-  },
-  faithForwardText: {
-    fontSize: 36,
-    fontWeight: "600",
-    // fontFamily: "Avenir",
-  },
-  forgotPassword: {},
   forgotPasswordButton: {
     marginVertical: 10,
   },
@@ -271,7 +243,6 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     color: "#808080",
   },
-  forgotPasswordView: {},
   input: {
     height: 50,
     width: width * 0.8,
@@ -286,9 +257,6 @@ const styles = StyleSheet.create({
   inputs: {
     paddingBottom: 10,
   },
-  linearGradient: {
-    flex: 1,
-  },
   logInButton: {
     backgroundColor: "white",
     borderWidth: 1,
@@ -301,8 +269,6 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     width: width * 0.7,
     height: width * 0.7,
-    /* width: 200, */
-    /* height: 200, */
   },
 });
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ffBlue: "#5eb5d1",
+        ffLightBlue: "#e6f5fa",
+        ffRed: "#ed6c61",
+        ffSand: "#DAC78E",
+        ffOrange: "#f08c7d",
+        ffPaper: "#fefdf9",
+        ffLightPaper: "rgba(255, 255, 255, 0.7)",
+        ffDarkPaper: "rgba(0, 0, 0, 0.05)",
+        ffBlack: "#222",
+      }
+    },
   },
   plugins: [],
 };
