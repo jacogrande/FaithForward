902c764359ca856ed39578af3f4799c46e972ec8
Jackson Prowell <jacksonprowell@proton.me>
2023-03-02 20:36:34 -0800

    Changed the background to be paper

--- src/screens/BibleNavigator.tsx
+++ src/screens/BibleNavigator.tsx
@@ -1,13 +1,22 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import ExegesisScreen from "@src/screens/ExegesisScreen";
 import { ReaderAndStudyNavigator } from "@src/screens/ReaderAndStudyNavigator";
+import colors from "@src/styles/colors";
 import * as React from "react";
 
 const Stack = createStackNavigator();
 
 const BaseNavigator: React.FC = () => {
   return (
-    <Stack.Navigator initialRouteName="Reader">
+    <Stack.Navigator
+      initialRouteName="Reader"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.paper,
+          shadowColor: "transparent",
+        },
+      }}
+    >
       <Stack.Screen
         name="ReaderAndStudy"
         component={ReaderAndStudyNavigator}
